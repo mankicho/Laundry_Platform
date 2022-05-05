324e73a0eef965fb5bbee3d0992e41dab7d5d9e5
mankicho <https://github.com/mankicho>
2022-05-05 22:08:46 +0900

    activeMQ 기본구조

--- src/main/java/com/coders/laundry/configuration/ActiveMQConfig.java
+++ src/main/java/com/coders/laundry/configuration/ActiveMQConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.util.backoff.BackOff;
 import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
