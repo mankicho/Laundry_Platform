dcb16800c4cb7c605651e4f20a03ed3cf6f09a08
S-HoChan <hoo12044@gmail.com>
2022-06-12 13:55:23 +0900

    Create Service

--- src/main/java/com/coders/laundry/service/BoardService.java
+++ src/main/java/com/coders/laundry/service/BoardService.java
@@ -1,2 +1,14 @@
-package com.coders.laundry.service;public class BoardService {
+package com.coders.laundry.service;
+
+import com.coders.laundry.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    // todo. operate
 }
